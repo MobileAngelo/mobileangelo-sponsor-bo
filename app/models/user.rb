b3b41451f25419bcd8276cf26ce6927c96a653b9
CallMeSH <dumalin.ch@gmail.com>
2015-02-05 16:21:42 +0100

    Adds a User crud in the admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,22 @@ class User < ActiveRecord::Base
   validates :mobile, presence: true, :phony_plausible => true
   validates :firstname, presence: true
   validates :lastname, presence: true
+
+  def hash_id
+    User.hash_id_instance.encode(id)
+  end
+
+  scope :with_hash_id, -> (value) { where(id: User.decode_hash_id(value)) }
+
+  def self.ransackable_scopes(auth_object = nil)
+    [:with_hash_id]
+  end
+
+  def self.decode_hash_id(value)
+    hash_id_instance.decode(value)
+  end
+
+  def self.hash_id_instance 
+    Hashids.new(Rails.configuration.hashid.salt, Rails.configuration.hashid.minimum_length, Rails.configuration.hashid.authorized)
+  end
 end
