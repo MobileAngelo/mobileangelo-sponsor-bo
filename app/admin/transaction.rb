2dca1f50e0bedfd581f659748f77bc5c82ef8c16
CallMeSH <dumalin.ch@gmail.com>
2015-02-06 11:50:58 +0100

    Add validations and form to transaction model

--- app/admin/transaction.rb
+++ app/admin/transaction.rb
@@ -1,3 +1,18 @@
 ActiveAdmin.register Transaction do
   belongs_to :user
+
+  form do |f|
+    inputs 'Details' do
+      input :recipient_id, as: :hidden
+      input :amount
+      input :sender_name
+      input :sender_mobile
+      input :sender_mail
+    end
+    actions
+  end
+
+  permit_params do
+    [:recipient_id, :amount, :sender_name, :sender_mail, :sender_mobile]
+  end  
 end
