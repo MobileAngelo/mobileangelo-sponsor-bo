27440bbc31282d8cb384995df45dc2290671c937
CallMeSH <dumalin.ch@gmail.com>
2015-02-06 12:12:48 +0100

    Improve list and filters for transactions

--- app/admin/transaction.rb
+++ app/admin/transaction.rb
@@ -1,6 +1,22 @@
 ActiveAdmin.register Transaction do
   belongs_to :user
 
+  index do
+    selectable_column
+
+    column :amount do |transaction|
+      number_to_currency(transaction.amount, locale: :fr)
+    end
+    column :sender_name
+    column :sender_mail
+    column :sender_mobile do |transaction|
+      transaction.sender_mobile.phony_formatted
+    end
+    #column "Sponsor Code", :hash_id
+    
+    actions
+  end
+
   form do |f|
     inputs 'Details' do
       input :recipient_id, as: :hidden
@@ -15,4 +31,10 @@ ActiveAdmin.register Transaction do
   permit_params do
     [:recipient_id, :amount, :sender_name, :sender_mail, :sender_mobile]
   end  
+
+  filter :amount
+  filter :sender_name
+  filter :sender_mail
+  filter :sender_mobile
+  filter :created_at
 end
